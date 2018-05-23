fabc4e2c36897491ad219abca64fc5c4e9ccba8f
Katya Horton <ekaterina.horton@gmail.com>
2018-05-23 11:36:22 +0100

    removing the js bugs

--- js/app.js
+++ js/app.js
@@ -22,9 +22,27 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+
+// ** create player class 
+
+const Player = function() {
+	
+}
+
+// *** create instance of player
+
+var player = new Player();
+
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// *** define allEnemies array 
+var allEnemies = [];
+
+// *** create instance of enemy and push it to allEnemies array 
+
+var enemy = new Enemy();
+allEnemies.push(enemy);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
